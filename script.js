99e3c8b1bee8f731e7f5873b8858cfb444a61fe3
Jeremy <jeremy.beaussart@gmail.com>
2026-04-13 16:49:46 +0200

    Update planet information with additional details on orbital periods. Enhanced descriptions for Mercury, Venus, Earth, Moon, Mars, Jupiter, Saturn, Uranus, Neptune, and Pluto to include the duration of their respective years.

--- script.js
+++ script.js
@@ -48,23 +48,23 @@ const INFO_PLACEHOLDER = '<p class="info__hint">Cliquez sur une planète</p>';
 const planetInfos = {
   sun: '<h2>Soleil</h2><ul><li>Type: Étoile</li><li>Surnom: Le Roi Soleil</li><li>Masse: 1,989 &times; 10<sup>30</sup> kg</li><li>Diamètre: 1 392 700 km</li></ul>',
   mercury:
-    '<h2>Mercure</h2><ul><li>Type: Planète rocheuse</li><li>Surnom: La Planète de Fer</li><li>Masse: 3,30 &times; 10<sup>23</sup> kg</li><li>Diamètre: 4 879 km</li><li>Distance par rapport au soleil: 58 millions km</li></ul>',
+    "<h2>Mercure</h2><ul><li>Type: Planète rocheuse</li><li>Surnom: La Planète de Fer</li><li>Masse: 3,30 &times; 10<sup>23</sup> kg</li><li>Diamètre: 4 879 km</li><li>Distance par rapport au soleil: 58 millions km</li><li>Durée d'une année: 88 jours terrestres</li></ul>",
   venus:
-    '<h2>Vénus</h2><ul><li>Type: Planète rocheuse</li><li>Surnom: L’Étoile du Berger</li><li>Masse: 4,87 &times; 10<sup>24</sup> kg</li><li>Diamètre: 12 104 km</li><li>Distance par rapport au soleil: 108 millions km</li></ul>',
+    "<h2>Vénus</h2><ul><li>Type: Planète rocheuse</li><li>Surnom: L’Étoile du Berger</li><li>Masse: 4,87 &times; 10<sup>24</sup> kg</li><li>Diamètre: 12 104 km</li><li>Distance par rapport au soleil: 108 millions km</li><li>Durée d'une année: 225 jours terrestres</li></ul>",
   earth:
-    '<h2>Terre</h2><ul><li>Type: Planète rocheuse</li><li>Surnom: La Planète Bleue</li><li>Masse: 5,97 &times; 10<sup>24</sup> kg</li><li>Diamètre: 12 756 km</li><li>Distance par rapport au soleil: 150 millions km</li></ul>',
-  moon: '<h2>Lune</h2><ul><li>Type: Satellite naturel</li><li>Surnom: L’Astre de Nuit</li><li>Masse: 7,34 &times; 10<sup>22</sup> kg</li><li>Diamètre: 3 474 km</li><li>Distance par rapport au soleil: Env. 150 millions km</li></ul>',
-  mars: '<h2>Mars</h2><ul><li>Type: Planète rocheuse</li><li>Surnom: La Planète Rouge</li><li>Masse: 6,42 &times; 10<sup>23</sup> kg</li><li>Diamètre: 6 792 km</li><li>Distance par rapport au soleil: 228 millions km</li></ul>',
+    "<h2>Terre</h2><ul><li>Type: Planète rocheuse</li><li>Surnom: La Planète Bleue</li><li>Masse: 5,97 &times; 10<sup>24</sup> kg</li><li>Diamètre: 12 756 km</li><li>Distance par rapport au soleil: 150 millions km</li><li>Durée d'une année: 365,25 jours</li></ul>",
+  moon: "<h2>Lune</h2><ul><li>Type: Satellite naturel</li><li>Surnom: L’Astre de Nuit</li><li>Masse: 7,34 &times; 10<sup>22</sup> kg</li><li>Diamètre: 3 474 km</li><li>Distance par rapport au soleil: Env. 150 millions km</li><li>Durée d'une année: 27,3 jours (révolution autour de la Terre)</li></ul>",
+  mars: "<h2>Mars</h2><ul><li>Type: Planète rocheuse</li><li>Surnom: La Planète Rouge</li><li>Masse: 6,42 &times; 10<sup>23</sup> kg</li><li>Diamètre: 6 792 km</li><li>Distance par rapport au soleil: 228 millions km</li><li>Durée d'une année: 687 jours terrestres (environ 1,9 an)</li></ul>",
   jupiter:
-    '<h2>Jupiter</h2><ul><li>Type: Planète géante gazeuse</li><li>Surnom: La Reine des Planètes</li><li>Masse: 1,90 &times; 10<sup>27</sup> kg</li><li>Diamètre: 142 984 km</li><li>Distance par rapport au soleil: 778 millions km</li></ul>',
+    "<h2>Jupiter</h2><ul><li>Type: Planète géante gazeuse</li><li>Surnom: La Reine des Planètes</li><li>Masse: 1,90 &times; 10<sup>27</sup> kg</li><li>Diamètre: 142 984 km</li><li>Distance par rapport au soleil: 778 millions km</li><li>Durée d'une année: 12 ans terrestres</li></ul>",
   saturn:
-    '<h2>Saturne</h2><ul><li>Type: Planète géante gazeuse</li><li>Surnom: La Belle aux Anneaux</li><li>Masse: 5,68 &times; 10<sup>26</sup> kg</li><li>Diamètre: 120 536 km</li><li>Distance par rapport au soleil: 1,4 milliard km</li></ul>',
+    "<h2>Saturne</h2><ul><li>Type: Planète géante gazeuse</li><li>Surnom: La Belle aux Anneaux</li><li>Masse: 5,68 &times; 10<sup>26</sup> kg</li><li>Diamètre: 120 536 km</li><li>Distance par rapport au soleil: 1,4 milliard km</li><li>Durée d'une année: 29,5 ans terrestres</li></ul>",
   uranus:
-    '<h2>Uranus</h2><ul><li>Type: Planète géante de glace</li><li>Surnom: La Planète Inclinée</li><li>Masse: 8,68 &times; 10<sup>25</sup> kg</li><li>Diamètre: 51 118 km</li><li>Distance par rapport au soleil: 2,9 milliards km</li></ul>',
+    "<h2>Uranus</h2><ul><li>Type: Planète géante de glace</li><li>Surnom: La Planète Inclinée</li><li>Masse: 8,68 &times; 10<sup>25</sup> kg</li><li>Diamètre: 51 118 km</li><li>Distance par rapport au soleil: 2,9 milliards km</li><li>Durée d'une année: 84 ans terrestres</li></ul>",
   neptune:
-    '<h2>Neptune</h2><ul><li>Type: Planète géante de glace</li><li>Surnom: La Planète des Vents</li><li>Masse: 1,02 &times; 10<sup>26</sup> kg</li><li>Diamètre: 49 528 km</li><li>Distance par rapport au soleil: 4,5 milliards km</li></ul>',
+    "<h2>Neptune</h2><ul><li>Type: Planète géante de glace</li><li>Surnom: La Planète des Vents</li><li>Masse: 1,02 &times; 10<sup>26</sup> kg</li><li>Diamètre: 49 528 km</li><li>Distance par rapport au soleil: 4,5 milliards km</li><li>Durée d'une année: 165 ans terrestres</li></ul>",
   pluton:
-    '<h2>Pluton</h2><ul><li>Type: Planète naine</li><li>Surnom: L’Exilée</li><li>Masse: 1,31 &times; 10<sup>22</sup> kg</li><li>Diamètre: 2 376 km</li><li>Distance par rapport au soleil: 5,9 milliards km</li></ul>',
+    "<h2>Pluton</h2><ul><li>Type: Planète naine</li><li>Surnom: L’Exilée</li><li>Masse: 1,31 &times; 10<sup>22</sup> kg</li><li>Diamètre: 2 376 km</li><li>Distance par rapport au soleil: 5,9 milliards km</li><li>Durée d'une année: 248 ans terrestres</li></ul>",
 };
 
 const planets = document.querySelectorAll('.planets');
